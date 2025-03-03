6795d64acd7666e3c96139ca836540bfe6ba1bba
Shreyas <shreyasjaiswal1515@gmail.com>
2025-03-03 14:06:08 +0530

    started with React

---
+++ REACT/app.js
@@ -0,0 +1,12 @@
+import reactDOM from "react-dom";
+import React from "react";
+
+const domRoot = document.getElementById('parent');
+const reactRoot = ReactRoot.createRoot(domRoot);
+
+const title = <h1>hellloooooo</h1>
+
+reactRoot.render(title);
+
+
+
---
+++ REACT_EXTRA DAY 01/1.js
@@ -0,0 +1,105 @@
+// const parent = document.getElementById("parent");
+// parent.innerHTML = "ITEM LIST";
+// const ul = document.createElement('ul');
+// const li1 = document.createElement('li');
+// li1.innerHTML = "item 1";
+// const li2 = document.createElement('li');
+// li2.innerHTML = "item 2";
+// const li3 = document.createElement('li');
+// li3.innerHTML = "item 3";
+
+// ul.appendChild(li1);
+// ul.appendChild(li2);
+// ul.appendChild(li3);
+// parent.appendChild(ul);
+//
+//          window
+//          document
+//          html
+//   head            body
+//              div       script
+//           #parent
+//
+//
+//
+//
+
+// console.log(React);
+// console.log(ReactDOM);
+
+// const domRoot = document.getElementById("parent");
+// const reactRoot = ReactDOM.createRoot(domRoot);
+
+// const li1 = React.createElement(
+//   "li",
+//   {
+//     style: {
+//       color: "red",
+//     },
+//   },
+//   "Item 1"
+// );
+// const li2 = React.createElement(
+//   "li",
+//   {
+//     className: "div",
+//   },
+//   "Item 2"
+// );
+// const li3 = React.createElement("li", {}, "Item 3"); // type , options , children
+
+// const ul = React.createElement("ul", {}, [li1, li2, li3]);
+// reactRoot.render(ul);
+//
+//
+//
+//
+//
+//         
+//
+// 
+//
+
+
+
+// const domRoot = document.getElementById("parent");
+// const reactRoot = ReactDOM.createRoot(domRoot);
+
+// const title = React.createElement("ul", {}, "Hello from React DOM");
+
+// console.log("type of title :", typeof title);
+// console.log("title",title);
+
+// const title2 = {
+//     $$typeof: Symbol.for("react.element"),
+//     "type": "h2",
+//     "props": {
+//         style:{color:"brown"},
+//       "children": "Hello from React DOM"
+//     },  
+   
+//   }
+
+
+// reactRoot.render(title2);
+//
+//
+//
+//
+//
+//   Hyphan case in html
+//   Camel case in JS
+//   Pascal case in React (Camel case + first letter capital);
+//
+//
+// const domRoot = document.getElementById("parent");
+// const reactRoot = ReactDOM.createRoot(domRoot);
+
+// const title2 = <h1>hello from JSX</h1>
+
+// reactRoot.render(title2);
+
+
+
+
+
